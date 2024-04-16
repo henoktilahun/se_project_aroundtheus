da6bb8386c57dbbe36df31fe88b48dd9eb695b95
Henok Tilahun <henok.tilahun1996@gmail.com>
2024-04-15 22:18:10 -0400

    rendered cards, updated template

--- scripts/index.js
+++ scripts/index.js
@@ -25,6 +25,7 @@ let initialCards = [
   },
 ];
 
+// Get all elements needed
 const editButton = document.querySelector("#profile-edit-button");
 const closeButton = document.querySelector("#modal-close-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
@@ -33,29 +34,51 @@ const profileSubtitle = document.querySelector(".profile__subtitle");
 const profileTitleInput = document.querySelector("#profile-title-input");
 const profileSubtitleInput = document.querySelector("#profile-subtitle-input");
 const profileEditForm = profileEditModal.querySelector(".modal__form");
-
+const cardListElement = document.querySelector(".cards__list");
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
 
+//closes modal
 function closePopup() {
   profileEditModal.classList.remove("modal_opened");
 }
 
-editButton.addEventListener("click", function () {
-  profileEditModal.classList.add("modal_opened");
-  profileTitleInput.value = profileTitle.textContent;
-  profileSubtitleInput.value = profileSubtitle.textContent;
-});
-
-closeButton.addEventListener("click", function () {
-  closePopup();
-});
-
-profileEditForm.addEventListener("submit", handleProfileFormSubmit);
-
+//handle when modal is saved
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   profileTitle.textContent = profileTitleInput.value;
   profileSubtitle.textContent = profileSubtitleInput.value;
   closePopup();
 }
+
+//gets card data and returns card elements cloned from template
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const cardImageElement = cardElement.querySelector(".card__image");
+  const cardTitleElement = cardElement.querySelector(".card__subheading");
+
+  cardImageElement.src = cardData.link;
+  cardImageElement.alt = cardData.name;
+  cardTitleElement.textContent = cardData.name;
+
+  return cardElement;
+}
+
+//listens for click event on modal and updates values of input
+editButton.addEventListener("click", function () {
+  profileEditModal.classList.add("modal_opened");
+  profileTitleInput.value = profileTitle.textContent;
+  profileSubtitleInput.value = profileSubtitle.textContent;
+});
+
+//listens to click event on pop up and handles it
+closeButton.addEventListener("click", closePopup);
+//listens to sumbit event and handles it
+profileEditForm.addEventListener("submit", handleProfileFormSubmit);
+
+//iterates through cards, gets card elemens and addes them to the HTML
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListElement.append(cardElement);
+});
